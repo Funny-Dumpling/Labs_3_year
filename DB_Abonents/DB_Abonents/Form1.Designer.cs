04dc544bf65321893e58602b0e3be06c228756c9
Funny-Dumpling <wertypokamogy@gmail.com>
2022-12-06 18:23:57 +0300

    Dop_lab
    Tables Abonent and tables Uslugi.
    Two forms - 
    first form - tables Abonents
    two form - tables Uslugi

---
+++ DB_Abonents/DB_Abonents/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DB_Abonents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dB_DMDataSet = new DB_Abonents.DB_DMDataSet();
+            this.abonentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.abonentTableAdapter = new DB_Abonents.DB_DMDataSetTableAdapters.abonentTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passportDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomerdogovoraDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.licevschetDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_DMDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.abonentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Услуги";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.passportDataGridViewTextBoxColumn,
+            this.nomerdogovoraDataGridViewTextBoxColumn,
+            this.licevschetDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.abonentBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 167);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Абонент";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dB_DMDataSet
+            // 
+            this.dB_DMDataSet.DataSetName = "DB_DMDataSet";
+            this.dB_DMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // abonentBindingSource
+            // 
+            this.abonentBindingSource.DataMember = "abonent";
+            this.abonentBindingSource.DataSource = this.dB_DMDataSet;
+            // 
+            // abonentTableAdapter
+            // 
+            this.abonentTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // passportDataGridViewTextBoxColumn
+            // 
+            this.passportDataGridViewTextBoxColumn.DataPropertyName = "passport";
+            this.passportDataGridViewTextBoxColumn.HeaderText = "passport";
+            this.passportDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.passportDataGridViewTextBoxColumn.Name = "passportDataGridViewTextBoxColumn";
+            this.passportDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nomerdogovoraDataGridViewTextBoxColumn
+            // 
+            this.nomerdogovoraDataGridViewTextBoxColumn.DataPropertyName = "nomer_dogovora";
+            this.nomerdogovoraDataGridViewTextBoxColumn.HeaderText = "nomer_dogovora";
+            this.nomerdogovoraDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nomerdogovoraDataGridViewTextBoxColumn.Name = "nomerdogovoraDataGridViewTextBoxColumn";
+            this.nomerdogovoraDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // licevschetDataGridViewTextBoxColumn
+            // 
+            this.licevschetDataGridViewTextBoxColumn.DataPropertyName = "licev_schet";
+            this.licevschetDataGridViewTextBoxColumn.HeaderText = "licev_schet";
+            this.licevschetDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.licevschetDataGridViewTextBoxColumn.Name = "licevschetDataGridViewTextBoxColumn";
+            this.licevschetDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_DMDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.abonentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private DB_DMDataSet dB_DMDataSet;
+        private System.Windows.Forms.BindingSource abonentBindingSource;
+        private DB_DMDataSetTableAdapters.abonentTableAdapter abonentTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passportDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomerdogovoraDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn licevschetDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ DB_Abonents/DB_Abonents/Form2.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DB_Abonents
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dB_DMDataSet = new DB_Abonents.DB_DMDataSet();
+            this.dBDMDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.услугиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.услугиTableAdapter = new DB_Abonents.DB_DMDataSetTableAdapters.УслугиTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.номерзаявкиDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.состояниеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.видуслугиDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_DMDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDMDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.услугиBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(379, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Услуги";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.номерзаявкиDataGridViewTextBoxColumn,
+            this.состояниеDataGridViewTextBoxColumn,
+            this.видуслугиDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.услугиBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(641, 124);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dB_DMDataSet
+            // 
+            this.dB_DMDataSet.DataSetName = "DB_DMDataSet";
+            this.dB_DMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dBDMDataSetBindingSource
+            // 
+            this.dBDMDataSetBindingSource.DataSource = this.dB_DMDataSet;
+            this.dBDMDataSetBindingSource.Position = 0;
+            // 
+            // услугиBindingSource
+            // 
+            this.услугиBindingSource.DataMember = "Услуги";
+            this.услугиBindingSource.DataSource = this.dBDMDataSetBindingSource;
+            // 
+            // услугиTableAdapter
+            // 
+            this.услугиTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // номерзаявкиDataGridViewTextBoxColumn
+            // 
+            this.номерзаявкиDataGridViewTextBoxColumn.DataPropertyName = "номер_заявки";
+            this.номерзаявкиDataGridViewTextBoxColumn.HeaderText = "номер_заявки";
+            this.номерзаявкиDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.номерзаявкиDataGridViewTextBoxColumn.Name = "номерзаявкиDataGridViewTextBoxColumn";
+            this.номерзаявкиDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // состояниеDataGridViewTextBoxColumn
+            // 
+            this.состояниеDataGridViewTextBoxColumn.DataPropertyName = "состояние";
+            this.состояниеDataGridViewTextBoxColumn.HeaderText = "состояние";
+            this.состояниеDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.состояниеDataGridViewTextBoxColumn.Name = "состояниеDataGridViewTextBoxColumn";
+            this.состояниеDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // видуслугиDataGridViewTextBoxColumn
+            // 
+            this.видуслугиDataGridViewTextBoxColumn.DataPropertyName = "вид_услуги";
+            this.видуслугиDataGridViewTextBoxColumn.HeaderText = "вид_услуги";
+            this.видуслугиDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.видуслугиDataGridViewTextBoxColumn.Name = "видуслугиDataGridViewTextBoxColumn";
+            this.видуслугиDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_DMDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDMDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.услугиBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dBDMDataSetBindingSource;
+        private DB_DMDataSet dB_DMDataSet;
+        private System.Windows.Forms.BindingSource услугиBindingSource;
+        private DB_DMDataSetTableAdapters.УслугиTableAdapter услугиTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn номерзаявкиDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn состояниеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn видуслугиDataGridViewTextBoxColumn;
+    }
+}
